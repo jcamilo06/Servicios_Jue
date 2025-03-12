003294a5d23abc658f8f217a4d3bef5b7c4d8817
Juan Camilo <168985090+jcamilo06@users.noreply.github.com>
2025-03-12 11:17:55 -0500

    Se crea la clase TipoProducto

---
+++ Clases/clsTipoProducto.cs
@@ -0,0 +1,55 @@
+﻿using Servicios_Jue.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace Servicios_Jue.Clases
+{
+    public class clsTipoProducto
+    {
+        private DBSuperEntities dbSuper = new DBSuperEntities();
+        public TIpoPRoducto tipoProducto { get; set; }
+        public List<TIpoPRoducto> ConsultarTodos()
+        {
+            return dbSuper.TIpoPRoductoes.ToList();
+        }
+        public TIpoPRoducto Consultar(int Codigo)
+        {
+            return dbSuper.TIpoPRoductoes.Where(x => x.Codigo == Codigo).FirstOrDefault();
+        }
+        public string Insertar()
+        {
+            try
+            {
+                dbSuper.TIpoPRoductoes.Add(tipoProducto); // Agrega el objeto empleado a la lista de productos. Todavía no se agrega a la base de datos. Se debe invocar el método saveChanges
+                dbSuper.SaveChanges();
+                return "Tipo de producto insertado correctamente";
+            }
+            catch(Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+        public string Actualizar()
+        {
+            try
+            {
+                TIpoPRoducto tipoProd = Consultar(tipoProducto.Codigo);
+                if (tipoProd == null)
+                {
+                    return "El tipo producto no existe en la base de datos";
+                }
+                tipoProd.Nombre = tipoProducto.Nombre;
+                tipoProd.Nombre = tipoProducto.Nombre;
+                dbSuper.SaveChanges();
+                return "Tipo de producto actualizado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+    }
+}
